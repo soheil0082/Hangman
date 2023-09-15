f8d1bb101c9b37cee9f4c5962c5d48ac7bc2cb71
soheil bayani <baayani.2.82@gmail.com>
2023-09-15 15:19:37 +0330

    Connects game over design and logic

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,11 +1,19 @@
+let container = document.querySelector(".container");
 let mainPanel = document.querySelector(".menu");
 let modePanel = document.querySelector(".modeMenu");
 let gamePanel = document.querySelector(".game");
+let endPanel = document.querySelector(".end");
+
 const apiLink = "https://www.wordgamedb.com/api/v1/words/";
+
+let winSFX = new Audio("SFX/success-fanfare-trumpets-6185.mp3");
+let looseSFX = new Audio("SFX/negative_beeps-6008.mp3");
+let wrongSFX = new Audio("SFX/wrong-38598.mp3");
 
 mainPanel.addEventListener("click", mainBtnHandler);
 modePanel.addEventListener("click", modeBtnHandler);
 gamePanel.addEventListener("click", gameBtnHandler);
+endPanel.addEventListener("click", endBtnHandler);
 
 function mainBtnHandler(event) {
   let text = event.target.innerText;
@@ -26,7 +34,27 @@ function modeBtnHandler(event) {
   newGame(text);
 }
 
+function endBtnHandler(event) {
+  let text = event.target.innerText.toLowerCase();
+
+  if (text == "home") {
+    endPanel.style.display = "none";
+    mainPanel.style.display = "grid";
+    container.style.height = "90vh";
+  } else if (text == "next") {
+    endPanel.style.display = "none";
+    container.style.height = "90vh";
+    newGame();
+  }
+}
+
 function newGame(Category) {
+  let elements = document.querySelectorAll(".disabled");
+
+  elements.forEach((element) => {
+    element.classList.remove("disabled");
+  });
+
   localStorage.setItem("wrongAnswer", 0);
 
   if (Category != "all" && Category != null) {
@@ -86,21 +114,44 @@ function gameBtnHandler(event) {
 
   if (localStorage.getItem("currentWord") == guess) {
     console.log("You Won");
+    winSFX.play();
+    endGame("w");
   }
 }
 
 function onWrongAnswer(answer) {
+  wrongSFX.play();
   let currentWrong = JSON.parse(localStorage.getItem("wrongAnswer")) + 1;
   localStorage.setItem("wrongAnswer", currentWrong);
   let image = gamePanel.querySelector("img");
 
-  answer.classList.add("false");
-  answer.addEventListener("animationend", (e) => {
-    answer.classList.remove("false");
-  });
+  if (currentWrong < 5) {
+    answer.classList.add("false");
+    answer.addEventListener("animationend", (e) => {
+      answer.classList.remove("false");
+    });
+  }
   image.src = `Img/${currentWrong}.png`;
   if (currentWrong >= 5) {
     console.log("loose");
+    looseSFX.play();
+    endGame("l");
+  }
+}
+
+function endGame(state) {
+  gamePanel.style.display = "none";
+  endPanel.style.display = "grid";
+  container.style.height = "fit-content";
+
+  if (state == "w") {
+    endPanel.querySelector("img").src = "Img/spongebob-victory-screech.gif";
+    endPanel.querySelector("p").innerText = "";
+  } else {
+    endPanel.querySelector("img").src = "Img/lost-the.gif";
+    endPanel.querySelector(
+      "p"
+    ).innerText = `The correct word is ${localStorage.getItem("currentWord")}`;
   }
 }
 
